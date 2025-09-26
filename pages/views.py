c2d767a3c70f8d2b4b75c76f3a5f8cdf746461ca
mym <maria.my.yim@gmail.com>
2025-09-26 14:24:59 +0800

    step 6 : navbar links update

--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 #    return HttpResponse("<h1>Hello, world !</h1>")
 
 def index(request):
+    #print(request.path)
     return render(request, 'pages/index.html')
 
 def about(request):
+    #print(request.path)
     return render(request,'pages/about.html')
